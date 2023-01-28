72f22d0f08f27a11b22e66498c420fe78456a95f
Afatsum2 <musaksac@gmail.com>
2023-01-28 23:14:21 +0300

    excel_data

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,10 +1,8 @@
 package pages;
-
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
-
 public class LoginPage {
     public LoginPage(){
         PageFactory.initElements(Driver.getDriver(),this);
@@ -15,5 +13,6 @@ public class LoginPage {
     public WebElement password;
     @FindBy(xpath = "//button[@type='submit']")
     public WebElement loginButton;
-
 }
+
+
