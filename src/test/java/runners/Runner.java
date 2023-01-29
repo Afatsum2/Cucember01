902a47de8db46d6ef42363b8053ccca7db8b6007
Afatsum2 <musaksac@gmail.com>
2023-01-29 22:57:32 +0300

    parallel testing

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -1,9 +1,7 @@
 package runners;
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
-
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
@@ -11,14 +9,14 @@ import org.junit.runner.RunWith;
                 "html:target/default-cucumber-reports.html",
                 "json:target/json-reports/cucumber.json",
                 "junit:target/xml-report/cucumber.xml",
-                "rerun:target/failedRerun.txt"
+                "rerun:target/failedRerun.txt",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
         },
         monochrome=true,
         features = "./src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
         dryRun = false,
         tags = "@failed_tests"
-
 )
 public class Runner {
 }
@@ -28,3 +26,5 @@ public class Runner {
  dryRun = false //IF dryRun = true. RUN DRY. JUST GIVE ME THE MISSING STEP DEFINITIONS. DON'T RUN THE TEST CASES.
  dryRun= false when we are doing test execution. dryRun=true when we are generation missing step definitions
 */
+
+
